ca81597174839070582c1c18e5df7aa8ce796ee4
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-10-13 11:16:07 +0500

    Add function to find matches in the table

--- src/main.py
+++ src/main.py
@@ -12,8 +12,17 @@ def load_watchlist(file_path):
         except Exception:
             raise FileNotFoundError("File not found or is not accessable.")
 
+def find_matches(table, title):
+    matches = table[table['Title'].str.contains(title, case=False, na=False)]
+    if matches.empty:
+        return None
+    else:
+        return matches[['Title', 'Title Type', 'Year', 'Genres']]
+
 def main():
-    table = load_watchlist("list.csv")
+    table = load_watchlist("watchlist.csv")
+    matches = find_matches(table, "simpsons")
+    print(matches)
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
